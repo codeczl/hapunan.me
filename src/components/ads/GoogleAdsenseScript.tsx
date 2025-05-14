38c38dec6de03df420adffadfaf5a4336fe9805f
codeczl <105200426+codeczl@users.noreply.github.com>
2025-05-14 12:09:01 +0800

    Update GoogleAdsenseScript.tsx

--- src/components/ads/GoogleAdsenseScript.tsx
+++ src/components/ads/GoogleAdsenseScript.tsx
@@ -7,9 +7,9 @@ const googleAdsenseId = process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID!;
 const GoogleAdsenseScript = () => {
   return (
     <Script
-      async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${googleAdsenseId}`}
-      crossOrigin="anonymous"
+      async 
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8919061004428483"
+      crossorigin="anonymous"
     />
   )
 }
